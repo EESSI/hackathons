d4902d58441bf3d7477d0ed7e0fb0f6fb91f3132
kerica <ericabianco.eb@gmail.com>
2021-12-16 10:54:59 +0100

    init tmp build_on_top

---
+++ 2021-12/02_software_on_top/build_on_top_eessi.tmp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## USAGE ##
+# build_on_top_eessi.tmp.sh $ARCH $REPO
+# where ARCH is the architecture in use
+# where REPO is the repo you have your recipe(s) and patch(es)
+# example:
+# ARCH="linux/x86_64/intel/haswell"
+# REPO="/mnt/shared/home/kErica/EB_TEST/repo/"
+
+
+
+ARCH=$2 # linux/x86_64/intel/haswell
+MYREPO_NAME=$1
+archsElements=(${ARCH//\// })
+OS=${archsElements[0]}
+SUBARCH1=${archsElements[1]}
+SUBARCH2=${archsElements[2]}
+SUBARCH3=${archsElements[3]}
+eb_version=eb --version | cut -f 4 -d ' '
+source /cvmfs/pilot.eessi-hpc.org/2021.06/compat/$OS/$SUBARCH1/bin/bash
+
+ml EasyBuild
+eb_version=eb --version | cut -f 4 -d ' '
+
+# Changes in EasyBuild configuration to install in the proper arch
+export EASYBUILD_INSTALLPATH="/cvmfs/pilot.eessi-hpc.org/2021.06/software/$ARCH"
+export EASYBUILD_ROBOT="$MYREPO_NAME"
+
+
