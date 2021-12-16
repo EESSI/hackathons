5d3a397c6c40b708dd957c172084bc17794ed8c9
ocaisa <a.ocais@fz-juelich.de>
2021-12-16 14:56:57 +0100

    Initial commit of this concept

---
+++ 2021-12/05_gpu/add_gpu_support.sh
@@ -0,0 +1,44 @@
+# Drop into the prefix shell
+# (or more likely pipe this script into a Prefix shell)
+
+# Set up the minimum EESSI environment variables
+# (this is using `latest` but we should actually be using a fixed version like $EESSI_PILOT_VERSION)
+source /cvmfs/pilot.eessi-hpc.org/latest/init/minimal_eessi_env
+
+##############################################################################################
+# Check if the CUDA compat libraries are installed and compatible with the target CUDA version
+# if not find the latest version of the compatibility libraries and install them
+
+# Create a general space for our NVIDIA compat drivers
+mkdir -p /cvmfs/pilot.eessi-hpc.org/host_injections/nvidia
+cd /cvmfs/pilot.eessi-hpc.org/host_injections/nvidia
+
+# Grab the latest compat library RPM
+# (this needs separate scripting as we need the real time latest version)
+wget https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-compat-11-5-495.29.05-1.x86_64.rpm
+
+# Unpack it
+# (the requirements here are OS dependent, can we get around that?)
+rpm2cpio cuda-compat-11-5-495.29.05-1.x86_64.rpm | cpio -idmv
+mv usr/local/cuda-11.5 .
+rm -r usr
+
+# Add a symlink that points to the latest version
+ln -sf cuda-11.5 latest
+
+# Create the space to host the libraries
+mkdir -p /cvmfs/pilot.eessi-hpc.org/host_injections/2021.06/compat/linux/x86_64
+# Symlink in the path to the latest libraries
+ln -s /cvmfs/pilot.eessi-hpc.org/host_injections/nvidia/latest/compat /cvmfs/pilot.eessi-hpc.org/host_injections/2021.06/compat/linux/x86_64/lib
+###############################################################################################
+
+###############################################################################################
+# Install CUDA
+# - we do not build the module (it already exists in EESSI), only install the package in the right location
+
+# Test building something with CUDA and running
+
+
+# Test a CUDA-enabled module from EESSI
+# (WIP)
+###############################################################################################
